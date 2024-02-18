5e0255820ab59ea3566d634b8a78331e1f60238e
Alessio Manià <alessio.mania311@gmail.com>
2024-02-18 16:37:04 +0100

    New class

---
+++ src/main/java/NewClass.java
@@ -0,0 +1,18 @@
+/**
+ * @author Alessio Manià - IN2000247
+ */
+public class NewClass {
+	private int number;
+
+	public NewClass(int number) {
+		this.number = number;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+}
